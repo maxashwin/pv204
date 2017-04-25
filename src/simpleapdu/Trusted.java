6f039f7d07513675ceac551e33906fa18fbac49c
maxashwin@yahoo.com <>
2017-04-26 01:39:05 +0200

    Update

--- src/simpleapdu/Trusted.java
+++ src/simpleapdu/Trusted.java
@@ -40,7 +40,7 @@ public class Trusted {
         (byte) 0x40, (byte) 0x41, (byte) 0x42, (byte) 0x43, (byte) 0x44, (byte) 0x45, (byte) 0x46, (byte) 0x47,
         (byte) 0x38, (byte) 0x39, (byte) 0x40, (byte) 0x41, (byte) 0x42, (byte) 0x43, (byte) 0x44, (byte) 0x45};
      
-    private static final byte[] cryptPassword = KEY_16;
+    private static final byte[] LONGTERMKEY = KEY_16;
      
     void generateHMAC(byte[] buffer) throws NoSuchAlgorithmException {
         
@@ -58,7 +58,7 @@ public class Trusted {
 
         byte[] key = new byte[dataLen];
         
-        key = cryptPassword;
+        key = LONGTERMKEY;
 
         for (short i = 0; i < dataLen; i++) {
             ipadK[i] = (byte) (key[i] ^ ipad);
@@ -216,21 +216,21 @@ public class Trusted {
                 //Take a password and Set it in the Card in Trusted Environment
           
                 //TO BE CHANGED.................USE A HASH FUNCTION TO HASH THE PASSWORD BEFORE SETTING
-                //byte[] cryptPassword = secretKey.getEncoded(); 
-                //byte[] cryptPassword = KEY_16;
+                //byte[] LONGTERMKEY = secretKey.getEncoded(); 
+                //byte[] LONGTERMKEY = KEY_16;
 
-                //System.out.println("Encryption Decryption PW :" + cardManager.bytesToHex(cryptPassword));
+                //System.out.println("Encryption Decryption PW :" + cardManager.bytesToHex(LONGTERMKEY));
                 
-                additionalDataLen = (short) cryptPassword.length;
+                additionalDataLen = (short) LONGTERMKEY.length;
                 apdu = new byte[CardMngr.HEADER_LENGTH + additionalDataLen];
                 apdu[CardMngr.OFFSET_CLA] = (byte) 0xB0;
-                apdu[CardMngr.OFFSET_INS] = (byte) 0x52; //52 is for Setting the Encryption and Decrytption Key
+                apdu[CardMngr.OFFSET_INS] = (byte) 0x62; //52 is for Setting the Encryption and Decrytption Key
                 apdu[CardMngr.OFFSET_P1] = (byte) 0x10;
                 apdu[CardMngr.OFFSET_P2] = (byte) 0x00;
                 apdu[CardMngr.OFFSET_LC] = (byte) additionalDataLen;             
 
                 if (additionalDataLen != 0){
-                    System.arraycopy(cryptPassword, 0, apdu, CardMngr.OFFSET_DATA, cryptPassword.length);
+                    System.arraycopy(LONGTERMKEY, 0, apdu, CardMngr.OFFSET_DATA, LONGTERMKEY.length);
                 }
 
                 if (cardManager.ConnectToCard()) {
@@ -253,168 +253,13 @@ public class Trusted {
                 System.out.println();
                 System.out.println("************************************************************");
                 if((byteResponse[0]==-112) &&  (byteResponse[1] == 0)){
-                    System.out.println("Setting of Encryption/ Decryption Key successful");
+                    System.out.println("Setting of Long Term Key successful");
                 }
                 else
-                    System.out.println("Setting of Encryption/ Decryption Key Unsuccessful");
+                    System.out.println("Setting of Long Term Key Unsuccessful");
                 System.out.println("************************************************************");
                 System.out.println();
 
-                /******************Get the Password stored in JavaCard to encrypt/decrypt the file ****************************/
-                /*
-                additionalDataLen = 0;
-                apdu = new byte[CardMngr.HEADER_LENGTH + additionalDataLen];
-                apdu[CardMngr.OFFSET_CLA] = (byte) 0xB0;
-                apdu[CardMngr.OFFSET_INS] = (byte) 0x53; 
-                apdu[CardMngr.OFFSET_P1] = (byte) 0x10;
-                apdu[CardMngr.OFFSET_P2] = (byte) 0x00;
-                apdu[CardMngr.OFFSET_LC] = (byte) additionalDataLen;   
-                
-                if (additionalDataLen != 0){
-                    System.arraycopy(cryptPassword, 0, apdu, CardMngr.OFFSET_DATA, cryptPassword.length);
-                }
-                
-                if (cardManager.ConnectToCard()) {
-                    
-                    cardManager.sendAPDU(SELECT_SIMPLEAPPLET);
-                    
-                    response = cardManager.sendAPDU(apdu);
-
-                    cardManager.DisconnectFromCard();
-                } else {
-                    System.out.println("Failed to connect to card");
-                }
-
-                byteResponse = response.getBytes();
-                
-                if((byteResponse[byteResponse.length-1]==-112) &&  (byteResponse[byteResponse.length] == 0)){
-                    System.out.println("Getting of Encryption/ Decryption Key successful");
-                }
-                else
-                    System.out.println("Getting of Encryption/ Decryption Key Unsuccessful");
-               
-                
-                //*************************************************************************************************
-
-                byte[] byteDataToEncrypt = TESTDATA;
-                                
-                //**************** AES Encryption ***************
-                
-                                
-                final int KEYLENGTH = 128;	
-		byte[] iv = new byte[KEYLENGTH / 8];
-                
-		SecureRandom prng;
-                prng = new SecureRandom();
-		prng.nextBytes(iv);
-                
-                //String siv = new String(iv);
-                System.out.println("IV =====" + cardManager.bytesToHex(iv));
-                
-                Cipher aesCipherForEncryption = Cipher.getInstance("AES/CBC/NoPadding");
-                
-                SecretKey dataKey = new SecretKeySpec(cryptPassword, 0, cryptPassword.length, "AES");
-                
-                aesCipherForEncryption.init(Cipher.ENCRYPT_MODE, dataKey, new IvParameterSpec(iv));
-                        
-		byte[] byteCipherText = aesCipherForEncryption.doFinal(byteDataToEncrypt);
-                
-                String strCipherText = new String(byteCipherText);
-		System.out.println("Cipher Text generated using AES is " + strCipherText);
-                
-                //****************Send the Encrypted Data to Card **********
-                
-                additionalDataLen =  (short)(byteCipherText.length + iv.length);
-                
-                apdu = new byte[CardMngr.HEADER_LENGTH + additionalDataLen];
-                apdu[CardMngr.OFFSET_CLA] = (byte) 0xB0;
-                apdu[CardMngr.OFFSET_INS] = (byte) 0x5B; //52 is for Setting the Encryption and Decrytption Key
-                apdu[CardMngr.OFFSET_P1] = (byte) 0x10;
-                apdu[CardMngr.OFFSET_P2] = (byte) 0x00;
-                apdu[CardMngr.OFFSET_LC] = (byte) additionalDataLen;    
-                
-
-                if (additionalDataLen != 0){
-                    System.arraycopy(byteCipherText, 0, apdu, CardMngr.OFFSET_DATA, byteCipherText.length);
-                    System.arraycopy(iv, 0, apdu, (short)(CardMngr.OFFSET_DATA + byteCipherText.length), iv.length);
-                }
-
-                if (cardManager.ConnectToCard()) {
-
-                    cardManager.sendAPDU(SELECT_SIMPLEAPPLET);
-
-                    response = cardManager.sendAPDU(apdu);
-
-                    cardManager.DisconnectFromCard();
-                } else {
-                    System.out.println("Failed to connect to card");
-                }
-
-                byteResponse = response.getBytes();
-
-                if((byteResponse[0]==-112) &&  (byteResponse[1] == 0)){
-                    System.out.println("Setting of Encryption/ Decryption Key successful");
-                }
-                else
-                    System.out.println("Setting of Encryption/ Decryption Key Unsuccessful");
-
-                
-                
-                //*************************************************************************************************
-
-                //******************Get the Password stored in JavaCard to encrypt/decrypt the file ****************
-                
-                
-                
-                
-                additionalDataLen = 0;
-                apdu = new byte[CardMngr.HEADER_LENGTH + additionalDataLen];
-                apdu[CardMngr.OFFSET_CLA] = (byte) 0xB0;
-                apdu[CardMngr.OFFSET_INS] = (byte) 0x53; 
-                apdu[CardMngr.OFFSET_P1] = (byte) 0x10;
-                apdu[CardMngr.OFFSET_P2] = (byte) 0x00;
-                apdu[CardMngr.OFFSET_LC] = (byte) additionalDataLen;   
-                
-                if (additionalDataLen != 0){
-                    System.arraycopy(cryptPassword, 0, apdu, CardMngr.OFFSET_DATA, cryptPassword.length);
-                }
-                
-                if (cardManager.ConnectToCard()) {
-                    
-                    cardManager.sendAPDU(SELECT_SIMPLEAPPLET);
-                    
-                    response = cardManager.sendAPDU(apdu);
-
-                    cardManager.DisconnectFromCard();
-                } else {
-                    System.out.println("Failed to connect to card");
-                }
-
-                byteResponse = response.getBytes();
-
-                
-                byte[] pw = new byte[20];
-                //for (int i=0; i<32; i++)
-                  //  pw[i] = byteResponse[i+5];
-                
-                System.arraycopy(byteResponse, 0, pw, 0, 20);
-                System.out.println("success1" + byteResponse.length);
-                String password = new String(pw);
-                System.out.println("success2");
-                
-                String fromPath1 = "/home/swatch/Desktop/Security_Technologies/Project/3.png";
-                String toPath1 ="/home/swatch/Desktop/Security_Technologies/Project/3.aes";
-                
-                AESCrypt aes = new AESCrypt(true, password);
-                aes.encrypt(2, fromPath1, toPath1);
-		System.out.println("success");
-                
-                String fromPath2 = "/home/swatch/Desktop/Security_Technologies/Project/3.aes";
-                String toPath2 ="/home/swatch/Desktop/Security_Technologies/Project/3decrypt.png";
-                aes.decrypt(fromPath2, toPath2);*/
-                
-                
-                
         } catch (Exception ex) {
             System.out.println("Exception : " + ex);
         }
